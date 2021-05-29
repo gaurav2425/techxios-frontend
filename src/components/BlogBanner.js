d4a56d57493812e759be50bacc8aa0010898aa5b
Gaurav <gauravburande04@gmail.com>
2021-05-29 21:06:01 +0530

    Styling navbar changed

--- src/components/BlogBanner.js
+++ src/components/BlogBanner.js
@@ -38,7 +38,7 @@ function BlogBanner() {
       <div className="date_time">
         <h4>
           {Day[currDay]}, {Months[currMonth]} {currDate}, {currYear} | DIAMOND
-          Skulls
+          SkULLS
         </h4>
       </div>
     </div>
